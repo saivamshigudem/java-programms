08e36514cfaf8ff7e0a4b8df739df61bb249f2a2
Sai Vamshi <94056823+saivamshigudem@users.noreply.github.com>
2022-10-18 22:35:12 +0530

    Create Value().java

---
+++ Value().java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		Integer a = new Integer(130);
+		System.out.println(a.doubleValue());
+		System.out.println(a.intValue());
+		System.out.println(a.byteValue());
+		System.out.println(a.floatValue());
+		System.out.println(a.shortValue());
+		
+}
+}
+
+//
+output
+130
+130
+-126
+130.0
+130
